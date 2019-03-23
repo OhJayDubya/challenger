688fe63e223e205c159bbe8f0d7713bbd3471b52
Unknown <ohjaydubya@gmail.com>
2019-03-23 09:56:15 +0000

    Reviews to data controller
    Added in the reviews to the data controller for both seeding and deleting

--- data/data.controller.js
+++ data/data.controller.js
@@ -9,15 +9,18 @@ mongoose.connect(process.env.DATABASE, {
 
 const User = require('../models/user.model');
 const Challenge = require('../models/challenge.model');
+const Review = require('../models/review.model');
 
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
 const challenges = JSON.parse(fs.readFileSync(`${__dirname}/challenges.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 async function deleteData() {
   console.warn('⚠️ Data being deleted from database');
 
   await User.deleteMany();
   await Challenge.deleteMany();
+  await Review.deleteMany();
 
   console.log('✅ Data has been deleted');
   process.exit();
@@ -29,6 +32,7 @@ async function loadData() {
 
     await User.insertMany(users);
     await Challenge.insertMany(challenges);
+    await Review.insertMany(reviews);
 
     console.log('✅ Data has been seeded');
     process.exit();
