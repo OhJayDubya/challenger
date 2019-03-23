a44eb92c113ef561a15a85f40a8d1ed0e7a42634
Unknown <ohjaydubya@gmail.com>
2019-03-23 09:29:57 +0000

    Made amendments to the data controller
    Added in the challenges into the data controller as well as refined the creation process to clear the database before seeding to prevent duplicate errors

--- data/data.controller.js
+++ data/data.controller.js
@@ -8,14 +8,31 @@ mongoose.connect(process.env.DATABASE, {
 });
 
 const User = require('../models/user.model');
+const Challenge = require('../models/challenge.model');
 
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const challenges = JSON.parse(fs.readFileSync(`${__dirname}/challenges.json`, 'utf-8'));
+
+async function deleteData() {
+  console.warn('⚠️ Data being deleted from database');
+
+  await User.deleteMany();
+  await Challenge.deleteMany();
+
+  console.log('✅ Data has been deleted');
+  process.exit();
+}
 
 async function loadData() {
   try {
+    console.log('♻️ Clearing database for seed')
+
+    deleteData();
+
     console.log('🌱 Seeding data into database');
 
     await User.insertMany(users);
+    await Challenge.insertMany(challenges);
 
     console.log('✅ Data has been seeded');
     process.exit();
@@ -23,15 +40,6 @@ async function loadData() {
     console.log(e);
     process.exit();
   }
-}
-
-async function deleteData() {
-  console.warn('⚠️ Data being deleted from database');
-
-  await User.remove();
-
-  console.log('✅ Data has been deleted');
-  process.exit();
 }
 
 if (process.argv.includes('--delete')) {
