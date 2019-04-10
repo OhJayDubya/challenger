8fbdb0079706f518fdb9cc23a545a71436827696
Unknown <ohjaydubya@gmail.com>
2019-04-10 21:49:27 +0100

    Created Globals
    Created javascript to handle global functions and libraries throughout the application

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const globals = require('./public/javascripts/globals');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -33,6 +34,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  res.locals.g = globals;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
