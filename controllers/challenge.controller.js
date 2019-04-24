d4baed0d5004a485d5727063b4c246cca0b97724
Unknown <ohjaydubya@gmail.com>
2019-04-24 23:19:35 +0100

    Created get challenge controller
    Created controller for the get challenge which will return a single random challenge from the database

--- controllers/challenge.controller.js
+++ controllers/challenge.controller.js
@@ -6,3 +6,11 @@ exports.getChallenges = async (req, res) => {
   const challenges = await Challenge.find();
   res.json(challenges);
 };
+
+exports.getChallenge = async (req, res) => {
+  const challenge = await Challenge
+    .aggregate([
+      { $sample: { size: 1 } },
+    ]);
+  res.send(challenge);
+};
